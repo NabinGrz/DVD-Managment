426767c46a22b5dd0ddf3bafa12ec28084028664
Suhant <72059795+Wolly-code@users.noreply.github.com>
2022-05-07 09:26:11 +0545

    Done FUNCTION 2

--- RopeyDVDs/Controllers/FunctionControllers/AssistantController.cs
+++ RopeyDVDs/Controllers/FunctionControllers/AssistantController.cs
@@ -17,6 +17,62 @@ namespace RopeyDVDs.Controllers
         {
             _dbcontext = dbcontext;
         }
+
+        //Function 3
+        //https://localhost:7135/NormalUser/GetDateOut?memNumber=3
+        public IActionResult GetDateOut(int memberNumber)
+        {
+            /**
+select distinct DT.DVDTitleName,DC.CopyNumber,L.DateOut,DATEDIFF(DAY,L.DateOut,'2022-04-14') aS dIFF from DVDTitles dt
+inner join DVDCopys dc 
+on dt.DVDNumber = dc.DVDNumber
+inner join Loans l
+on dc.CopyNumber = l.CopyNumber
+inner join CastMembers c
+on c.DVDNumber = dc.DVDNumber
+inner join Members m
+on l.MemberNumber = m.MembershipNumber
+where (L.DateOut >= (GETDATE()-31) and m.MembershipNumber = 3);
+
+             DateTime currentDate = DateTime.Now.Date;
+            */
+
+            DateTime currentDate = DateTime.Now.Date;
+            DateTime lastDate = currentDate.Subtract(new TimeSpan(31, 0, 0, 0, 0));
+            Console.WriteLine("============================================================");
+            Console.WriteLine("CURRENT" + currentDate);
+            Console.WriteLine("LAST DATE" + lastDate);
+
+            var dvdTitle = _dbcontext.DVDTitles.ToList();
+            var dvdCopy = _dbcontext.DVDCopys.ToList();
+            var castMember = _dbcontext.CastMembers.ToList();
+            var member = _dbcontext.Members.ToList();
+            var loan = _dbcontext.Loans.ToList();
+
+
+            var details = from d in dvdTitle
+                          join dc in dvdCopy
+                          on d.DVDNumber equals dc.DVDNumber into table1
+                          from dc in table1.ToList().Distinct().Where(dc => dc.DVDNumber == d.DVDNumber)
+                          join l in loan on dc.CopyNumber equals l.CopyNumber into table2
+                          from l in table2.ToList().Distinct().Where(l => l.CopyNumber == dc.CopyNumber)
+                          join c in castMember
+                          on dc.DVDNumber equals c.DVDNumber into table3
+                          from c in table3.ToList().Distinct().Where(c => c.DVDNumber == dc.DVDNumber)
+                          join m in member
+                          on l.MemberNumber equals m.MembershipNumber into table4
+                          from m in table4.ToList().Distinct().Where(m => m.MembershipNumber == l.MemberNumber && m.MembershipNumber == memberNumber && DateTime.Parse(l.DateOut) >= lastDate)
+                          select new { dvdTitle = d, castMember = c, dvdCopy = dc, loan = l, member = m };
+
+            //var r = _context.Actors.FirstOrDefault();
+            //ViewBag.last = r;
+            ViewBag.date = details;
+            ViewBag.memberNumbers = _dbcontext.Members.ToArray();
+            return View(details);
+        }
+
+
+
         //FUNCTION 4
         //https://localhost:44344/Assistant/GetList
         public IActionResult GetList()
--- RopeyDVDs/Controllers/FunctionControllers/NormalUserController.cs
+++ RopeyDVDs/Controllers/FunctionControllers/NormalUserController.cs
@@ -43,74 +43,35 @@ namespace RopeyDVDs.Controllers
         [Authorize]
        public IActionResult GetActorCopy(String surname)
         {
+            /**
+             select dt.DVDTitleName,(dc.Stock - Count(*)) as "Dif(Stock Left)" from  loans l
+            inner join DVDCopys dc
+            on dc.CopyNumber = l.CopyNumber
+            inner join DVDTitles dt
+            on dt.DVDNumber = dc.DVDNumber
+            group by l.CopyNumber, dc.Stock,dt.DVDTitleName
+             * */
             ViewBag.actorName = _context.Actors.ToArray();
             String n = "0";
             var dvdTitle = _context.DVDTitles.ToList();
             var dvdCopy = _context.DVDCopys.ToList();
             var castMember = _context.CastMembers.ToList();
             var actorDetails = _context.Actors.ToList();
-            var loan = _context.Loans.ToList();
-            var details = (from a in actorDetails
-                          join c in castMember on a.ActorNumber equals c.ActorNumber
-                          join dt in dvdTitle on c.DVDNumber equals dt.DVDNumber
-                          join dc in dvdCopy on dt.DVDNumber equals dc.DVDNumber
-                          //join l in loan on dc.CopyNumber equals l.CopyNumber
-                          select new { dvdTitle = dt, castMember = c,dvdCopy = dc, actorDetails = a }).Distinct().Where(x => x.actorDetails.ActorSurname == surname && x.dvdCopy.Stock >=1 );
+            var loan = _context.Loans.ToList(); var details = from l in loan
+                          join dc in dvdCopy on l.CopyNumber equals dc.CopyNumber
+                          join dt in dvdTitle on dc.DVDNumber equals dt.DVDNumber
+                          group new { l, dc, dt } by new { l.CopyNumber, dc.Stock, dt.DVDTitleName }
+                          into grp
+                          select new
+                          {
+                              DVDTitleName = grp.Key.DVDTitleName,
+                              Stock = grp.Key.Stock,
+                              LoanCount = grp.Count(),
+                          };
             ViewBag.name = details;
             //Console.WriteLine(ViewBag.name);
             return View(details);
         }
-        //Function 3
-        //https://localhost:7135/NormalUser/GetDateOut?memNumber=3
-        public IActionResult GetDateOut(int memberNumber)
-        {
-            /**
-select distinct DT.DVDTitleName,DC.CopyNumber,L.DateOut,DATEDIFF(DAY,L.DateOut,'2022-04-14') aS dIFF from DVDTitles dt
-inner join DVDCopys dc 
-on dt.DVDNumber = dc.DVDNumber
-inner join Loans l
-on dc.CopyNumber = l.CopyNumber
-inner join CastMembers c
-on c.DVDNumber = dc.DVDNumber
-inner join Members m
-on l.MemberNumber = m.MembershipNumber
-where (L.DateOut >= (GETDATE()-31) and m.MembershipNumber = 3);
-
-             DateTime currentDate = DateTime.Now.Date;
-            */
-
-            DateTime currentDate = DateTime.Now.Date;
-            DateTime lastDate = currentDate.Subtract(new TimeSpan(31, 0, 0, 0, 0));
-            Console.WriteLine("============================================================");
-            Console.WriteLine("CURRENT" + currentDate);
-            Console.WriteLine("LAST DATE" + lastDate);
-
-            var dvdTitle = _context.DVDTitles.ToList();
-            var dvdCopy = _context.DVDCopys.ToList();
-            var castMember = _context.CastMembers.ToList();
-            var member = _context.Members.ToList();
-            var loan = _context.Loans.ToList();
-
-
-            var details = from d in dvdTitle
-                          join dc in dvdCopy
-                          on d.DVDNumber equals dc.DVDNumber into table1
-                          from dc in table1.ToList().Distinct().Where(dc => dc.DVDNumber == d.DVDNumber)
-                          join l in loan on dc.CopyNumber equals l.CopyNumber into table2
-                          from l in table2.ToList().Distinct().Where(l => l.CopyNumber == dc.CopyNumber)
-                          join c in castMember
-                          on dc.DVDNumber equals c.DVDNumber into table3
-                          from c in table3.ToList().Distinct().Where(c => c.DVDNumber == dc.DVDNumber)
-                          join m in member
-                          on l.MemberNumber equals m.MembershipNumber into table4
-                          from m in table4.ToList().Distinct().Where(m => m.MembershipNumber == l.MemberNumber && m.MembershipNumber == memberNumber && DateTime.Parse(l.DateOut) >= lastDate)
-                          select new { dvdTitle = d, castMember = c, dvdCopy = dc, loan = l, member = m };
-
-            //var r = _context.Actors.FirstOrDefault();
-            //ViewBag.last = r;
-            ViewBag.date = details;
-            ViewBag.memberNumbers = _context.Members.ToArray();
-            return View(details);
-        }
+   
     }
 }
